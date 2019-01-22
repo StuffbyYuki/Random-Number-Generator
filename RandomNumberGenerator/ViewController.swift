b77a216dbee481d54a017c75d866235a414c97bf
Your Name <you@example.com>
2019-01-21 23:25:32 -0700

    Initial Commit

---
+++ RandomNumberGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomNumberGenerator
+//
+//  Created by 掛川優希 on 1/21/19.
+//  Copyright © 2019 Yuki Kakegawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
